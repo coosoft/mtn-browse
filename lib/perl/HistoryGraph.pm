513e33bb9a5927cdc1571913c587e90240a8a4d3
Anthony Edward Cooper <aecooper@cpan.org>
2011-12-27 14:00:07 +0000

    - Have to create the canvas myself as Glade/libglade does not honour the   anti-aliased setting. - Got the writing out to, calling of and reading back from dot working. - Also got basic rendering of the graph working as well.

--- lib/perl/HistoryGraph.pm
+++ lib/perl/HistoryGraph.pm
@@ -49,9 +49,11 @@ no warnings qw(recursion);
 
 # ***** GLOBAL DATA DECLARATIONS *****
 
-# Constants for the columns within the comparison files ListStore widget.
+# Constants for the scale of items in a graph.
 
-use constant CLS_FILE_NAME_COLUMN => 0;
+use constant DPI    => 72;
+use constant HEIGHT => 28;
+use constant WIDTH  => 72;
 
 # ***** FUNCTIONAL PROTOTYPES *****
 
@@ -62,8 +64,10 @@ sub display_history_graph($;$$$);
 # Private routines.
 
 sub build_ancestry_graph($$;$$$);
+sub draw_graph($);
 sub get_history_graph_window();
 sub graph_reconnect_helper($$);
+sub layout_graph($);
 #
 ##############################################################################
 #
@@ -117,9 +121,17 @@ sub display_history_graph($;$$$)
 
     $instance->{appbar}->set_status(__("Building ancestry graph"));
     $wm->update_gui();
-    build_ancestry_graph($instance, 1, $branches, $from_date, $to_date);
+    build_ancestry_graph($instance, 0, $branches, $from_date, $to_date);
 
     # get_revision_history_helper($instance, $revision_id);
+
+    $instance->{appbar}->set_status(__("Laying out graph with dot"));
+    $wm->update_gui();
+    layout_graph($instance);
+
+    $instance->{appbar}->set_status(__("Drawing graph"));
+    $wm->update_gui();
+    draw_graph($instance);
 
     $instance->{stop_button}->set_sensitive(FALSE);
     $wm->update_gui();
@@ -182,6 +194,10 @@ sub build_ancestry_graph($$;$$$)
        %graph_db,
        @head_revisions,
        $selected_set);
+    my $wm = WindowManager->instance();
+
+    $instance->{graph_data}->{child_graph} = {};
+    $instance->{graph_data}->{head_revisions} = [];
 
     # First build up revision hit lists based upon the selection criteria. This
     # will be then used when scanning the graph to weed out unwanted revisions.
@@ -288,8 +304,10 @@ sub build_ancestry_graph($$;$$$)
 
     $instance->{mtn}->graph(\@graph);
 
-    # Build up a revision parent/child graph indexed on revision id with only
-    # the revisions that we are interested in within.
+    $wm->update_gui();
+
+    # Build up a revision child graph indexed on revision id with only the
+    # revisions that we are interested in within.
 
     foreach my $entry (@graph)
     {
@@ -383,6 +401,9 @@ sub build_ancestry_graph($$;$$$)
 
     }
 
+    $wm->update_gui();
+    return if ($instance->{stop});
+
     # Now find out how many head revisions we have.
 
     foreach my $revision_id (keys(%graph_db))
@@ -437,6 +458,8 @@ sub build_ancestry_graph($$;$$$)
 	    $context->{outside_selection} = undef;
 	    $context->{parents} = [];
 	    graph_reconnect_helper($context, $head_id);
+	    $wm->update_gui();
+	    return if ($instance->{stop});
 	}
     }
 
@@ -618,6 +641,534 @@ sub graph_reconnect_helper($$)
 #
 ##############################################################################
 #
+#   Routine      - layout_graph
+#
+#   Description  - Given a child graph database, get dot to lay out the
+#                  geometric shapes that we want to render and then read back
+#                  the result.
+#
+#   Data         - $instance    : The history graph window instance.
+#                  Return Value : True if the generation worked, otherwise
+#                                 false if something went wrong.
+#
+##############################################################################
+
+
+
+sub layout_graph($)
+{
+
+    my $instance = $_[0];
+
+    my (@arrows,
+	$buffer,
+	$child_db,
+	@circles,
+	@err,
+	$fh_err,
+	$fh_in,
+	$fh_out,
+	$my_pid,
+	$pid,
+	$prev_lines,
+	@rectangles,
+	@revision_ids,
+	$stop,
+	$total_bytes,
+	$watcher);
+    my $wm = WindowManager->instance();
+
+    $instance->{graph_data}->{arrows} = [];
+    $instance->{graph_data}->{circles} = [];
+    $instance->{graph_data}->{rectangles} = [];
+    $instance->{graph_data}->{max_x} = 0;
+    $instance->{graph_data}->{max_y} = 0;
+
+    # Run the dot subprocess.
+
+    $fh_err = gensym();
+    $my_pid = $$;
+    eval
+    {
+	$pid = open3($fh_in,
+		     $fh_out,
+		     $fh_err,
+		     "dot", "-q", "-y", "-s72", "-Txdot");
+    };
+
+    # Check for errors (remember that open3() errors can happen in both the
+    # parent and child processes).
+
+    if ($@)
+    {
+	if ($$ != $my_pid)
+	{
+
+	    # In the child process so all we can do is complain and exit.
+
+	    warn(__x("open3 failed: {error_message}", error_message => $@));
+	    exit(1);
+
+	}
+	else
+	{
+
+	    # In the parent process so deal with the error in the usual way.
+
+	    my $dialog = Gtk2::MessageDialog->new
+		(undef,
+		 ["modal"],
+		 "warning",
+		 "close",
+		 __x("The dot subprocess could not start,\n"
+		         . "the system gave:\n<b><i>{error_message}</b></i>",
+		     error_message => Glib::Markup::escape_text($@)));
+	    WindowManager->instance()->allow_input(sub { $dialog->run(); });
+	    $dialog->destroy();
+	    return;
+
+	}
+    }
+
+    # Setup a watch handler to read our data when we hand control over to GTK2.
+
+    $total_bytes = 0;
+    $buffer = "";
+    $watcher = Gtk2::Helper->add_watch
+	($fh_out->fileno(), "in",
+	 sub {
+	     my $bytes_read;
+	     if (($bytes_read = $fh_out->sysread($buffer, 32768, $total_bytes))
+		     == 0
+		 || ! defined($bytes_read))
+	     {
+		 $stop = 1;
+	     }
+	     else
+	     {
+		 $total_bytes += $bytes_read;
+	     }
+	     return TRUE;
+	 });
+
+    # Generate a list of topographically sorted revision ids for each revision
+    # in the graph database.
+
+    $child_db = $instance->{graph_data}->{child_graph};
+    $instance->{mtn}->toposort(\@revision_ids, keys(%$child_db));
+
+    # Now write out the dot graph to the dot subprocess. We don't need to worry
+    # about reading and writing at the same time as dot needs the complete
+    # graph before it can do its magic.
+
+    # Pre-amble.
+
+    $fh_in->print("digraph \"mtn-browse\"\n"
+		  . "{\n"
+		  . "  graph [ranksep=\"0.25\"] ;\n"
+		  . "  node [label=\"\"] ;\n");
+
+    # Rectangular non-merge nodes.
+
+    $fh_in->printf("  node [shape=box, width = %f, height = %f] ;\n",
+		   WIDTH / DPI,
+		   HEIGHT / DPI);
+    foreach my $revision_id (@revision_ids)
+    {
+	if (! $child_db->{$revision_id}->{merge_node})
+	{
+	    $fh_in->print("  \"" . $revision_id . "\" ;\n");
+	}
+    }
+
+    # Circular merge nodes.
+
+    $fh_in->printf("  node [shape=circle, width = %f, height = %f] ;\n",
+		   HEIGHT / DPI,
+		   HEIGHT / DPI);
+    foreach my $revision_id (@revision_ids)
+    {
+	if ($child_db->{$revision_id}->{merge_node})
+	{
+	    $fh_in->print("  \"" . $revision_id . "\" ;\n");
+	}
+    }
+
+    # Head nodes.
+
+    $fh_in->print("  subgraph heads\n"
+		  . "  {\n"
+		  . "    rank = sink ;\n");
+    foreach my $revision_id (@{$instance->{graph_data}->{head_revisions}})
+    {
+	$fh_in->print("    \"" . $revision_id . "\" ;\n");
+    }
+    $fh_in->print("  }\n");
+
+    # Hierarchy.
+
+    foreach my $revision_id (@revision_ids)
+    {
+	foreach my $child_id (@{$child_db->{$revision_id}->{children}})
+	{
+	    $fh_in->print("  \"" . $revision_id . "\" -> \"" . $child_id
+			  . "\" ;\n");
+	}
+    }
+
+    # Close off the graph and close dot's input so that it knows to start
+    # processing the data.
+
+    $fh_in->print("}\n");
+    $fh_in->close();
+
+    # Hand control over to GTK2 whilst we read in the output from dot.
+
+    while (! $stop && ! $instance->{stop})
+    {
+	Gtk2->main_iteration();
+    }
+    Gtk2::Helper->remove_watch($watcher);
+    $buffer = "" if ($instance->{stop});
+
+    # If we have been asked to abort then terminate the subprocess, otherwise
+    # get any error output as the subprocess has just exited of its own accord.
+
+    if ($instance->{stop})
+    {
+	kill("TERM", $pid);
+    }
+    else
+    {
+	@err = $fh_err->getlines() unless ($instance->{stop});
+    }
+
+    $fh_out->close();
+    $fh_err->close();
+
+    # Reap the process and deal with any errors.
+
+    for (my $i = 0; $i < 4; ++ $i)
+    {
+
+	my $wait_status = 0;
+
+	# Wait for the subprocess to exit (preserving the current state of $@
+	# so that any exception that has already occurred is not lost, also
+	# ignore any errors resulting from waitpid() interruption).
+
+	{
+	    local $@;
+	    eval
+	    {
+		local $SIG{ALRM} = sub { die(WAITPID_INTERRUPT); };
+		alarm(5);
+		$wait_status = waitpid($pid, 0);
+		alarm(0);
+	    };
+	    $wait_status = 0
+		if ($@ eq WAITPID_INTERRUPT && $wait_status < 0
+		    && $! == EINTR);
+	}
+
+	# The subprocess has terminated.
+
+	if ($wait_status == $pid)
+	{
+	    if (! $instance->{stop})
+	    {
+		my $exit_status = $?;
+		if (WIFEXITED($exit_status) && WEXITSTATUS($exit_status) != 0)
+		{
+		    my $dialog = Gtk2::MessageDialog->new_with_markup
+			(undef,
+			 ["modal"],
+			 "warning",
+			 "close",
+			 __x("The dot subprocess failed with an exit status\n"
+				 . "of {exit_code} and printed the following "
+			         . "on stderr:\n"
+				 . "<b><i>{error_message}</i></b>",
+			     exit_code => WEXITSTATUS($exit_status),
+			     error_message => Glib::Markup::escape_text
+					      (join("", @err))));
+		    WindowManager->instance()->allow_input
+			(sub { $dialog->run(); });
+		    $dialog->destroy();
+		    return;
+		}
+		elsif (WIFSIGNALED($exit_status))
+		{
+		    my $dialog = Gtk2::MessageDialog->new
+			(undef,
+			 ["modal"],
+			 "warning",
+			 "close",
+			 __x("The dot subprocess was terminated by signal "
+				 . "{number}.",
+			     number => WTERMSIG($exit_status)));
+		    WindowManager->instance()->allow_input
+			(sub { $dialog->run(); });
+		    $dialog->destroy();
+		    return;
+		}
+	    }
+	    last;
+	}
+
+	# The subprocess is still there so try and kill it unless it's time to
+	# just give up.
+
+	elsif ($i < 3 && $wait_status == 0)
+	{
+	    if ($i == 0)
+	    {
+		kill("INT", $pid);
+	    }
+	    elsif ($i == 1)
+	    {
+		kill("TERM", $pid);
+	    }
+	    else
+	    {
+		kill("KILL", $pid);
+	    }
+	}
+
+	# Stop if we don't have any relevant children to wait for anymore.
+
+	elsif ($wait_status < 0 && $! == ECHILD)
+	{
+	    last;
+	}
+
+	# Either there is some other error with waitpid() or a child process
+	# has been reaped that we aren't interested in (in which case just
+	# ignore it).
+
+	elsif ($wait_status < 0)
+	{
+	    my $err_msg = $!;
+	    kill("KILL", $pid);
+	    my $dialog = Gtk2::MessageDialog->new_with_markup
+		(undef,
+		 ["modal"],
+		 "warning",
+		 "close",
+		 __x("waitpid failed with:\n<b><i>{error_message}</i></b>",
+		     error_message => Glib::Markup::escape_text($!)));
+	    WindowManager->instance()->allow_input(sub { $dialog->run(); });
+	    $dialog->destroy();
+	    return;
+	}
+
+    }
+
+    # Parse the dot output, line by line.
+
+    $prev_lines = "";
+    foreach my $line (split(/\n/, $buffer))
+    {
+
+	my $revision_id;
+
+	# Deal with statements that span multiple lines.
+
+	if ($line =~ m/^(.*)\\$/)
+	{
+	    $prev_lines .= $1;
+	    next;
+	}
+	elsif ($prev_lines ne "")
+	{
+	    $line = $prev_lines . $line;
+	    $prev_lines = "";
+	}
+
+	# Parse statements.
+
+	# Look out for revision ids, always take the first one.
+
+	if ($line =~ m/[ \t"]([0-9a-f]{40})[ \t"]/)
+	{
+	    $revision_id = $1;
+	}
+
+	# Boxes.
+
+	if ($line =~ m/p 4(( \d+){8}) *\"/)
+	{
+	    my ($br_x,
+		$br_y,
+		@coords,
+		$tl_x,
+		$tl_y);
+	    @coords = split(/ /, $1);
+	    shift(@coords);
+	    $tl_x = $instance->{graph_data}->{max_x} + 100;
+	    $br_x = 0;
+	    foreach my $i (0, 2, 4, 6)
+	    {
+		$tl_x = ($tl_x < $coords[$i]) ? $tl_x : $coords[$i];
+		$br_x = ($br_x > $coords[$i]) ? $br_x : $coords[$i];
+	    }
+	    $tl_y = $instance->{graph_data}->{max_y} + 100;
+	    $br_y = 0;
+	    foreach my $i (1, 3, 5, 7)
+	    {
+		$tl_y = ($tl_y < $coords[$i]) ? $tl_y : $coords[$i];
+		$br_y = ($br_y > $coords[$i]) ? $br_y : $coords[$i];
+	    }
+	    push(@rectangles, {revision_id => $revision_id,
+			       tl_x        => $tl_x,
+			       tl_y        => $tl_y,
+			       br_x        => $br_x,
+			       br_y        => $br_y});
+	}
+
+	# Lines with arrow heads.
+
+	elsif ($line =~ m/B \d+(( \d+ \d+)+) *\".* P 3(( \d+){6}) *\"/)
+	{
+	    my (@arrow_points,
+		@line_points);
+	    @line_points = split(/ /, $1);
+	    shift(@line_points);
+	    @arrow_points = split(/ /, $3);
+	    shift(@arrow_points);
+	    push(@arrows, {revision_id => $revision_id,
+			   line        => \@line_points,
+			   arrow       => \@arrow_points});
+	}
+
+	# Circles.
+
+	elsif ($line =~ m/e(( \d+){4}) *\"/)
+	{
+	    my @list;
+	    @list = split(/ /, $1);
+	    shift(@list);
+	    push(@circles, {revision_id => $revision_id,
+			    x           => $list[0],
+			    y           => $list[1],
+			    width       => $list[2],
+			    height      => $list[3]});
+	}
+
+	# Bounding box (i.e. the total size of the graph).
+
+	elsif ($line =~ m/bb=\"\d+,\d+,(\d+),(\d+)\"/)
+	{
+	    $instance->{graph_data}->{max_x} = $1;
+	    $instance->{graph_data}->{max_y} = $2;
+	}
+
+    }
+
+    $instance->{graph_data}->{arrows} = \@arrows;
+    $instance->{graph_data}->{circles} = \@circles;
+    $instance->{graph_data}->{rectangles} = \@rectangles;
+
+    return 1;
+
+}
+#
+##############################################################################
+#
+#   Routine      - draw_graph
+#
+#   Description  - Given a child graph database and the geometric objects laid
+#                  out by dot, draw the history graph in the canvas widget.
+#
+#   Data         - $instance    : The history graph window instance.
+#
+##############################################################################
+
+
+
+sub draw_graph($)
+{
+
+    my $instance = $_[0];
+
+    my $group;
+
+    $instance->{graph_canvas}->set_scroll_region
+	(0,
+	 0,
+	 $instance->{graph_data}->{max_x},
+	 $instance->{graph_data}->{max_y});
+
+    $group = Gnome2::Canvas::Item->new($instance->{graph_canvas}->root(),
+				       "Gnome2::Canvas::Group",
+				       x => 0,
+				       x => 0);
+    $instance->{graph_group} = $group;
+
+    foreach my $rectangle (@{$instance->{graph_data}->{rectangles}})
+    {
+	Gnome2::Canvas::Item->new($group,
+				  "Gnome2::Canvas::Rect",
+				  x1 => $rectangle->{tl_x},
+				  y1 => $rectangle->{tl_y},
+				  x2 => $rectangle->{br_x},
+				  y2 => $rectangle->{br_y},
+				  fill_color => "yellow",
+				  outline_color => "black",
+				  width_pixels => 2);
+    }
+
+    foreach my $arrow (@{$instance->{graph_data}->{arrows}})
+    {
+	my ($head,
+	    $bpath,
+	    $i,
+	    $line,
+	    $pathdef);
+	$head = $arrow->{arrow};
+	$line = $arrow->{line};
+	$pathdef = Gnome2::Canvas::PathDef->new();
+	$pathdef->moveto($$line[0], $$line[1]);
+	$i = 2;
+	for ($i = 2; $i < scalar(@$line); $i += 6)
+	{
+	    $pathdef->curveto($$line[$i],
+			      $$line[$i + 1],
+			      $$line[$i + 2],
+			      $$line[$i + 3],
+			      $$line[$i + 4],
+			      $$line[$i + 5]);
+	}
+	$pathdef->moveto($$head[0], $$head[1]);
+	$pathdef->lineto($$head[2], $$head[3]);
+	$pathdef->lineto($$head[4], $$head[5]);
+	$pathdef->closepath();
+	$bpath = Gnome2::Canvas::Item->new($group,
+					   "Gnome2::Canvas::Bpath",
+					   fill_color => "black",
+					   outline_color => "black",
+					   width_pixels => 2);
+	$bpath->set_path_def($pathdef);
+	$bpath->lower_to_bottom();
+    }
+
+    foreach my $circle (@{$instance->{graph_data}->{circles}})
+    {
+	Gnome2::Canvas::Item->new($group,
+				  "Gnome2::Canvas::Ellipse",
+				  x1 => $circle->{x} - $circle->{width},
+				  y1 => $circle->{y} - $circle->{height},
+				  x2 => $circle->{x} + $circle->{width},
+				  y2 => $circle->{y} + $circle->{height},
+				  fill_color => "pink",
+				  outline_color => "black",
+				  width_pixels => 2);
+    }
+
+}
+#
+##############################################################################
+#
 #   Routine      - get_history_graph_window
 #
 #   Description  - Creates or prepares an existing history graph window for
@@ -664,7 +1215,6 @@ sub get_history_graph_window()
 	$instance->{window} = $instance->{glade}->get_widget($window_type);
 	foreach my $widget ("appbar",
 			    "graph_scrolledwindow",
-			    "graph_canvas",
 			    "graph_button_vbox",
 			    "revision_change_history_button",
 			    "revision_change_log_button",
@@ -677,6 +1227,13 @@ sub get_history_graph_window()
 	    $instance->{$widget} = $instance->{glade}->get_widget($widget);
 	}
 
+	# Create the graph canvas widget. We can't do this is Glade as
+	# something does not honour the anti-aliased setting.
+
+	$instance->{graph_canvas} = Gnome2::Canvas->new_aa();
+	$instance->{graph_scrolledwindow}->add($instance->{graph_canvas});
+	$instance->{graph_canvas}->show_all();
+
 	# Setup the history callbacks.
 
 	$instance->{window}->signal_connect
@@ -687,6 +1244,9 @@ sub get_history_graph_window()
 		 local $instance->{in_cb} = 1;
 		 $widget->hide();
 		 # $instance->{history_buffer}->set_text("");
+		 $instance->{graph_group}->destroy()
+		     if (defined($instance->{graph_group}));
+		 $instance->{graph_data} = undef;
 		 $instance->{mtn} = undef;
 		 return TRUE;
 	     },
@@ -735,6 +1295,8 @@ sub get_history_graph_window()
 	$instance->{appbar}->clear_stack();
     }
 
+    $instance->{graph_group} = undef;
+    $instance->{graph_data} = undef;
     $instance->{stop} = 0;
 
     # Empty out the contents.
